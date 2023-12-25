1df135fd38d4712f8f62e92d7e8ca6b8244d8ced
Gravydigger <gravydigger65@gmail.com>
2023-12-25 20:34:17 +1030

    Set up for Github actions page deployment

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://blog.gravydigger.net/",
+	site: "https://gravydigger.net/",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		rehypePlugins: [
